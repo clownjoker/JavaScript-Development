648bb63b480baa7c1b4488548fdd84e7954b3817
Meliodas <elsiddig190@hotmail.com>
2018-05-02 19:36:23 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* global console */
+/*
+* here we are 
+* we just easly will do something in the begining of this project
+* there are a few steps we'll doing "Joker" 
+* the very first step is selecting elements
+* the second one is how to change these elements
+* then we'll try to crate elements from javascript file
+* thats all we'll do in a few days so keep well and start
+*/
