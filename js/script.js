42c73b7d1fb4f6fe837a329dbbbabbde74d919ee
Meliodas <elsiddig190@hotmail.com>
2018-05-05 13:39:52 +0200

    using selecting way to get the body

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 /* global console ,alert*/
 /*
-* here we are 
+* here we are
 * we just easly will do something in the begining of this project
-* there are a few steps we'll doing "Joker" 
+* there are a few steps we'll doing "Joker"
 * the very first step is selecting elements
 * the second one is how to change these elements
 * then we'll try to crate elements from javascript file
 * thats all we'll do in a few days so keep well and start
 */
+
+let bodyClass = document.getElementsByClassName('bodyClass'), // select the body by its class
+
+    bodyId = document.getElementById('bodyId'), // select the body by its id
+
+    bodyQuery = document.querySelector('body'); // using querySelector to select the body as element
+
+var selectByJquery = $("body"); // using jquery to select the body as element
+
+// leave a comment if anything is not understand
